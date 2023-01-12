d5f3623b8149b251e1e8e0ff7f03ba7ba882836f
Rhiannon <renii@reniivali.com>
2023-01-12 15:33:04 -0700

    hey look its a css thing

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
+const d = document;
+
+d.addEventListener('DOMContentLoaded', function() {
 	let navSection = 0;
-	let navSections = document.getElementsByClassName('section');
-	let navButtons = document.getElementsByClassName('navButton');
+	let navSections = d.getElementsByClassName('section');
+	let navButtons = d.getElementsByClassName('navButton');
 
 	function checkShown() {
 		for (let i = 0; i < navSections.length; i++) {
 			if (i === navSection) {
-				navSections[i].style.display = 'block';
+				navSections[i].style.display = '';
 				navButtons[i].style.backgroundColor = '#1e1e2e';
 				navButtons[i].style.color = '#a6adc8';
 				navButtons[i].style.borderRadius = '10px 0 0 10px';
@@ -32,7 +34,15 @@ document.addEventListener('DOMContentLoaded', function() {
 		});
 	}
 
+	//css dict stuff
+	const borderRadSlider = d.getElementById("borderRadDemoSlide")
+	const borderRadElement = d.getElementById("borderRadDemo")
+	borderRadSlider.addEventListener('input', function() {
+		borderRadElement.style.borderRadius = borderRadSlider.value + "px";
+		d.getElementById("borderRadDemoVal").innerHTML = `Radius: ${borderRadSlider.value}px`;
+	});
+
 	//clicker
-	gei('click').addEventListener('click', click.click);
-	click.newUpgrade("Mouse.Reinforcement", 10, 1.2, 0, 1);
+	//gei('click').addEventListener('click', click.click);
+	//click.newUpgrade("Mouse.Reinforcement", 10, 1.2, 0, 1);
 });
